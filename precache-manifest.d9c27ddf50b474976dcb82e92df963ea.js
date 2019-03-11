462b2f047b4429377ccf82b07fb0a83657e114e8
Rafał Kamiński <46964777+rafkami@users.noreply.github.com>
2019-03-11 10:12:52 +0100

    Updates

---
+++ precache-manifest.d9c27ddf50b474976dcb82e92df963ea.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "37e36d3f8baea113cfd3e1131180cf87",
+    "url": "/rafkami/weather-teller/static/media/rainbow-1909_1920.37e36d3f.jpg"
+  },
+  {
+    "revision": "3d7715acfd761002368c",
+    "url": "/rafkami/weather-teller/static/js/runtime~main.3d7715ac.js"
+  },
+  {
+    "revision": "853c4668bd6fc59be1ba",
+    "url": "/rafkami/weather-teller/static/js/main.853c4668.chunk.js"
+  },
+  {
+    "revision": "09932c71981d03c583b3",
+    "url": "/rafkami/weather-teller/static/js/2.09932c71.chunk.js"
+  },
+  {
+    "revision": "853c4668bd6fc59be1ba",
+    "url": "/rafkami/weather-teller/static/css/main.21d52133.chunk.css"
+  },
+  {
+    "revision": "0b08038372e0d092c3afaa8f9c43a02b",
+    "url": "/rafkami/weather-teller/index.html"
+  }
+];
